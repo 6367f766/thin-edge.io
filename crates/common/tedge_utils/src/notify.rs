671c600cdba647b7b27b4d419ed5d806a3b51536
alexandru solomes <solo@softwareag.com>
2022-10-20 13:11:45 +0100

    Add new notify crate
    Signed-off-by: alexandru solomes <solo@softwareag.com>

--- crates/common/tedge_utils/src/notify.rs
+++ crates/common/tedge_utils/src/notify.rs
@@ -1,319 +1,210 @@
-#[cfg(test)]
-mod tests {
-    use futures::{
-        channel::mpsc::{channel, Receiver},
-        SinkExt, StreamExt,
-    };
-    use notify::{Config, Error, Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
-    use std::{path::PathBuf, sync::Arc};
-    use tedge_test_utils::fs::TempTedgeDir;
+use std::{
+    collections::{HashMap, HashSet},
+    path::{Path, PathBuf},
+    time::Duration,
+};
 
-    use crate::file::create_file_with_user_group;
+use async_stream::try_stream;
+use futures::{
+    //channel::mpsc::{channel, Receiver, Sender},
+    SinkExt,
+    Stream,
+    StreamExt,
+};
+use notify::{
+    event::{AccessKind, AccessMode, CreateKind, RemoveKind},
+    Config, EventKind, INotifyWatcher, RecommendedWatcher, RecursiveMode, Watcher,
+};
+use tokio::sync::mpsc::{channel, Receiver};
+use try_traits::default::TryDefault;
 
-    async fn assert_stream(mut stream: Receiver<(PathBuf, EventKind)>) {
-        while let Some(event) = stream.next().await {
-            dbg!(event);
-        }
-    }
+use crate::fs_notify::{FileEvent, NotifyStreamError};
 
-    fn command(command: &str) {
-        let mut cmd = std::process::Command::new(command);
-        cmd.spawn().unwrap();
-    }
+type DirPath = PathBuf;
+type MaybeFileName = Option<String>;
+type Metadata = HashMap<DirPath, HashMap<MaybeFileName, HashSet<FileEvent>>>;
 
-    #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
-    async fn it_works_with_tokio() -> notify::Result<()> {
-        let ttd = Arc::new(TempTedgeDir::new());
-        let ttd_clone = ttd.clone();
-        dbg!(ttd.path());
-        let dir = ttd_clone.dir("dir_a");
-        let (mut watcher, mut rx) = async_watcher()?;
+struct WatchMetadata {
+    metadata: Metadata,
+}
 
-        watcher.watch(ttd.path().as_ref(), RecursiveMode::Recursive)?;
+struct NotifyStream {
+    watcher: INotifyWatcher,
+    rx: Receiver<(PathBuf, FileEvent)>,
+    metadata: WatchMetadata,
+}
 
-        let fs_handle = tokio::spawn(async move {
-            dbg!("this was called");
-            dir.file("file_a");
-            let new_dir = ttd_clone.dir("new_directory");
-            //std::fs::create_dir(ttd_clone.path().join("new_directory")).unwrap();
-            ttd_clone
-                .dir("new_directory")
-                .file("file_one")
-                .with_raw_content("file content");
-            ttd_clone.dir("new_directory").file("file_bb");
-            dir.file("file_b");
-            dir.file("file_c");
-            new_dir.file("file_aaaaa");
-        });
+impl TryDefault for NotifyStream {
+    type Error = NotifyStreamError;
 
-        let stream = tokio::spawn(async move {
-            while let Some(event) = rx.next().await {
-                dbg!(event);
-            }
-        });
-
-        fs_handle.await.unwrap();
-        stream.await.unwrap();
-        //fs_handle.await.unwrap();
-
-        Ok(())
-
-        //loop {
-        //    tokio::select! {
-        //        Some(event) = rx.next() => {
-        //            dbg!(event);
-        //        }
-        //    }
-        //}
-
-        //let add_watcher = tokio::spawn(async move {
-        //    let path = Path::new("/tmp/my-dir");
-        //    let result = watcher.watch(path.as_ref(), RecursiveMode::Recursive);
-        //    dbg!(result);
-        //});
-
-        //server_handler.await.unwrap();
-        //handle.await.unwrap();
-        //add_watcher.await.unwrap();
-
-        //Ok(())
-    }
-
-    fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<(PathBuf, EventKind)>)> {
-        let (mut tx, rx) = channel(10);
+    fn try_default() -> Result<Self, Self::Error> {
+        let (tx, rx) = channel(1024);
 
         // Automatically select the best implementation for your platform.
         // You can also access each implementation directly e.g. INotifyWatcher.
         let watcher = RecommendedWatcher::new(
-            move |res: Result<Event, Error>| {
+            move |res: Result<notify::Event, notify::Error>| {
                 futures::executor::block_on(async {
-                    let res = res.unwrap();
-                    for path in res.paths {
-                        tx.send((path, res.kind.clone())).await.unwrap();
+                    if let Ok(notify_event) = res {
+                        match notify_event.kind {
+                            EventKind::Access(AccessKind::Close(AccessMode::Write)) => {
+                                for path in notify_event.paths {
+                                    tx.send((path, FileEvent::Modified)).await.unwrap();
+                                }
+                            }
+                            EventKind::Create(CreateKind::File) => {
+                                for path in notify_event.paths {
+                                    tx.send((path, FileEvent::Created)).await.unwrap();
+                                }
+                            }
+                            EventKind::Create(CreateKind::Folder) => {
+                                for path in notify_event.paths {
+                                    tx.send((path, FileEvent::Created)).await.unwrap();
+                                }
+                            }
+                            //EventKind::Modify(_) => todo!(),
+                            EventKind::Remove(RemoveKind::File) => {
+                                for path in notify_event.paths {
+                                    tx.send((path, FileEvent::Deleted)).await.unwrap();
+                                }
+                            }
+                            EventKind::Remove(RemoveKind::Folder) => {
+                                for path in notify_event.paths {
+                                    tx.send((path, FileEvent::Deleted)).await.unwrap();
+                                }
+                            }
+                            _other => {}
+                        }
                     }
                 })
             },
-            Config::default().with_poll_interval(std::time::Duration::from_millis(10)),
-        )?;
-
-        Ok((watcher, rx))
+            Config::default(),
+        )
+        .unwrap();
+        Ok(Self {
+            watcher,
+            rx,
+            metadata: WatchMetadata {
+                metadata: HashMap::new(),
+            },
+        })
     }
-
-    //async fn async_watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
-    //    let (mut watcher, mut rx) = async_watcher()?;
-
-    //    // Add a path to be watched. All files and directories at that path and
-    //    // below will be monitored for changes.
-    //    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
-
-    //    while let Some(res) = rx.next().await {
-    //        match res {
-    //            Ok(event) => println!("changed: {:?}", event),
-    //            Err(e) => println!("watch error: {:?}", e),
-    //        }
-    //    }
-
-    //    Ok(())
-    //}
 }
 
-//use async_stream::try_stream;
-//use futures::Stream;
-//// This crate replaces fs_notify, using the `notify` crate: https://github.com/notify-rs/notify
-//use notify::{
-//    raw_watcher, watcher, DebouncedEvent, INotifyWatcher, RawEvent, RecursiveMode, Watcher,
-//};
-//use std::{
-//    collections::{HashMap, HashSet},
-//    path::Path,
-//    sync::{
-//        mpsc::{channel, Receiver, Sender},
-//        Arc,
-//    },
-//};
-//use std::{path::PathBuf, time::Duration};
-//
-//use crate::fs_notify::{FileEvent, NotifyStreamError};
-//
-//struct NotifyStream {
-//    rx: Receiver<DebouncedEvent>,
-//    watcher: INotifyWatcher,
-//    metadata: HashMap<PathBuf, HashSet<FileEvent>>,
-//    path_track: HashSet<PathBuf>,
-//}
-//
-//impl NotifyStream {
-//    pub fn add_watcher(
-//        &mut self,
-//        dir_path: &Path,
-//        file: Option<&str>,
-//        events: &[FileEvent],
-//    ) -> Result<(), NotifyStreamError> {
-//        let (path, mode) = if let Some(file) = file {
-//            (dir_path.join(file), RecursiveMode::NonRecursive)
-//        } else {
-//            (dir_path.to_path_buf(), RecursiveMode::Recursive)
-//        };
-//        self.watcher.watch(&path, mode).unwrap();
-//        let entry = self.metadata.entry(path).or_insert(HashSet::new());
-//        for event in events {
-//            entry.insert(*event);
-//        }
-//        Ok(())
-//    }
-//
-//    fn matches(&self, path: &Path, file_event: &FileEvent) -> Option<(PathBuf, FileEvent)> {
-//        let path_clone = path.clone().parent().unwrap();
-//        if path_clone.is_file() {
-//            // TODO
-//            None
-//        } else {
-//            // is dir
-//            //let parent = path.parent()?;
-//            let hs = self.metadata.get(path_clone).unwrap();
-//            if hs.contains(file_event) {
-//                Some((path.to_path_buf(), *file_event))
-//            } else {
-//                None
-//            }
-//        }
-//    }
-//
-//    fn stream<'a>(
-//        &'a mut self,
-//    ) -> impl Stream<Item = Result<(PathBuf, FileEvent), NotifyStreamError>> + 'a {
-//        try_stream! {
-//            loop {
-//                match self.rx.recv() {
-//                    Ok(event) => {
-//                        match event {
-//                            DebouncedEvent::NoticeWrite(path) => {
-//                                if let Some((path, file_event)) = self.matches(&path, &FileEvent::Modified) {
-//                                    yield (path, file_event)
-//                                }
-//                            }
-//                            DebouncedEvent::Create(path) => {
-//                                if self.path_track.contains(&path) {
-//                                    // the path was not new
-//                                } else {
-//                                    self.path_track.insert(path.clone());
-//                                    if let Some((path, file_event)) = self.matches(&path, &FileEvent::Created) {
-//                                        yield (path, file_event)
-//                                    }
-//                                }
-//                            }
-//                            DebouncedEvent::Remove(path) => {
-//                                if let Some((path, file_event)) = self.matches(&path, &FileEvent::Deleted) {
-//                                    yield (path, file_event)
-//                                }
-//                            }
-//                            _rest => {}
-//                        }
-//                    },
-//                    Err(_e) => yield Err(NotifyStreamError::FailedToCreateStream)?,
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//impl Default for NotifyStream {
-//    fn default() -> Self {
-//        let (tx, rx) = channel();
-//        let watcher = INotifyWatcher::new(tx, std::time::Duration::from_secs(0)).unwrap(); // TODO
-//        Self {
-//            rx,
-//            watcher,
-//            metadata: HashMap::new(),
-//            path_track: HashSet::new(),
-//        }
-//    }
-//}
-//
-//#[cfg(test)]
-//mod tests {
-//
-//    pub use futures::{pin_mut, Stream, StreamExt};
-//    use notify::{raw_watcher, watcher, RecursiveMode, Watcher};
-//    use std::sync::mpsc::channel;
-//    use std::time::Duration;
-//    use tedge_test_utils::fs::TempTedgeDir;
-//
-//    use crate::{fs_notify::FileEvent, notify::NotifyStream};
-//
-//    //use super::stream_notify_event;
-//
-//    #[tokio::test]
-//    async fn it_streams() {
-//        let ttd = TempTedgeDir::new();
-//        //ttd.file("file_a");
-//        dbg!(ttd.path());
-//
-//        let mut inotify_stream = NotifyStream::default();
-//        inotify_stream
-//            .add_watcher(
-//                ttd.path(),
-//                None,
-//                &[FileEvent::Created, FileEvent::Modified, FileEvent::Created],
-//            )
-//            .unwrap();
-//
-//        let stream = inotify_stream.stream();
-//        pin_mut!(stream);
-//
-//        //let file_handler = tokio::spawn(async move {
-//        //    ttd.file("file_a").with_raw_content("this is the content");
-//        //});
-//        //while let Some(Ok((path, flag))) = stream.next().await {
-//        //    dbg!(path, flag);
-//        //}
-//
-//        loop {
-//            tokio::select! {
-//                Some(Ok((path, flag))) = stream.next() => {
-//                    dbg!(path, flag);
-//                },
-//            }
-//        }
-//    }
-//
-//    #[tokio::test]
-//    async fn it_works() {
-//        let ttd = TempTedgeDir::new();
-//        // Create a channel to receive the events.
-//        let (tx, rx) = channel();
-//
-//        // Create a watcher object, delivering debounced events.
-//        // The notification back-end is selected based on the platform.
-//        let mut watcher = watcher(tx, Duration::from_nanos(10)).unwrap();
-//
-//        // Add a path to be watched. All files and directories at that path and
-//        // below will be monitored for changes.
-//        dbg!(ttd.path());
-//        watcher.watch(ttd.path(), RecursiveMode::Recursive).unwrap();
-//
-//        let spawn_handle = tokio::spawn(async move {
-//            let _handle = tokio::spawn(async move {
-//                loop {
-//                    match rx.recv() {
-//                        Ok(event) => println!("{:?}", event),
-//                        Err(e) => println!("watch error: {:?}", e),
-//                    }
-//                }
-//            });
-//        });
-//
-//        let fs_handle = tokio::spawn(async move {
-//            ttd.file("file_a");
-//            ttd.dir("dir_one");
-//            let ten_millis = std::time::Duration::from_millis(100);
-//            std::thread::sleep(ten_millis);
-//            ttd.dir("dir_one").dir("dir_two").file("file_c");
-//            let ten_millis = std::time::Duration::from_millis(100);
-//            std::thread::sleep(ten_millis);
-//        });
-//
-//        spawn_handle.await.unwrap();
-//        fs_handle.await.unwrap();
-//    }
-//}
+impl NotifyStream {
+    fn get_metadata_as_mut(&mut self) -> &mut Metadata {
+        &mut self.metadata.metadata
+    }
+
+    fn add_watcher(&mut self, dir_path: &Path, file: Option<String>, events: &[FileEvent]) {
+        // we make the full path for the watcher in case a `file` is given.
+        // when a `file` is given we return a NonRecursive watcher
+        let (full_path, recursive_mode) = if let Some(file_name) = file.clone() {
+            (dir_path.join(file_name), RecursiveMode::NonRecursive)
+        } else {
+            (dir_path.to_path_buf(), RecursiveMode::Recursive)
+        };
+
+        self.watcher.watch(&full_path, recursive_mode).unwrap(); // TODO
+
+        // we add the information to the metadata
+        let maybe_file_name_entry = self
+            .get_metadata_as_mut()
+            .entry(dir_path.to_path_buf())
+            .or_insert(HashMap::new());
+
+        let file_event_entry = maybe_file_name_entry.entry(file).or_insert(HashSet::new());
+        for event in events {
+            file_event_entry.insert(*event);
+        }
+    }
+
+    fn stream<'a>(
+        &'a mut self,
+    ) -> impl Stream<Item = Result<(PathBuf, FileEvent), NotifyStreamError>> + '_ {
+        try_stream! {
+            for (path, file_event) in &self.rx.recv().await {
+                yield  (path.to_owned(), *file_event)
+             }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::HashMap, path::PathBuf, sync::Arc, time::Duration};
+
+    use futures::{pin_mut, Stream, StreamExt};
+    use maplit::hashmap;
+    use tedge_test_utils::fs::TempTedgeDir;
+    use try_traits::default::TryDefault;
+
+    use crate::fs_notify::{FileEvent, NotifyStreamError};
+
+    use super::NotifyStream;
+
+    async fn assert_stream(
+        mut inputs: HashMap<String, Vec<FileEvent>>,
+        stream: impl Stream<Item = Result<(PathBuf, FileEvent), NotifyStreamError>>,
+    ) {
+        pin_mut!(stream);
+        while let Some(Ok((path, flag))) = stream.next().await {
+            let file_name = String::from(path.file_name().unwrap().to_str().unwrap());
+            let mut values = match inputs.get_mut(&file_name) {
+                Some(v) => v.to_vec(),
+                None => {
+                    inputs.remove(&file_name);
+                    continue;
+                }
+            };
+            match values.iter().position(|x| *x == flag) {
+                Some(i) => values.remove(i),
+                None => {
+                    continue;
+                }
+            };
+            if values.is_empty() {
+                inputs.remove(&file_name);
+            } else {
+                inputs.insert(file_name, values);
+            }
+            if inputs.is_empty() {
+                break;
+            }
+            dbg!(&inputs);
+        }
+    }
+
+    #[tokio::test]
+    async fn it_works() {
+        let ttd = Arc::new(TempTedgeDir::new());
+        let ttd_clone = ttd.clone();
+        let mut fs_notify = NotifyStream::try_default().unwrap();
+        fs_notify.add_watcher(ttd.path(), None, &[FileEvent::Created, FileEvent::Modified]);
+
+        let file_system_handler = tokio::spawn(async move {
+            ttd_clone.file("file_b"); //.with_raw_content("yo");
+            ttd_clone.file("file_a");
+            ttd_clone.file("file_c");
+            //tokio::time::sleep(Duration::from_secs(1)).await;
+            //ttd_clone.file("file_c").delete();
+        });
+
+        let fs_notify_handler = tokio::spawn(async move {
+            let stream = fs_notify.stream();
+            pin_mut!(stream);
+            while let Some(Ok((path, event))) = stream.next().await {
+                dbg!(path, event);
+            }
+        });
+
+        //let fs_notify_handler = tokio::task::spawn(async move {
+        //    assert_stream(expected_events, stream).await;
+        //});
+
+        //fs_notify_handler.await.unwrap();
+        file_system_handler.await.unwrap();
+    }
+}
+
+// TODO: explore https://github.com/notify-rs/notify/issues/380
